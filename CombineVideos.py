914a6f0bba1b59b6e06796fcd097db7e2293251e
Parker E <parkereischen@gmail.com>
2019-04-02 00:00:02 +0200

    Commented/Cleaned up code
    Removed previously tested videos as well

--- CombineVideos.py
+++ CombineVideos.py
@@ -1,34 +1,36 @@
 
-import sys, os, os.path
+import sys
+import os
+import os.path
 from moviepy.editor import *
 import random
 
 
 def combineVideos():
-	titles = os.listdir('videos')
-	random.shuffle(titles)
-	clips = []
-	for title in titles:
-		if title != '.DS_Store':
-			video = VideoFileClip('videos/'+title)
-			clips.append(video)
-	count = 0
-	for clip in clips:
-		if count !=0:
-			clip.crossfadein(1)
-		count+=1
-	titleClip = VideoFileClip('PowerHourIntro.mp4')
-	clips = [titleClip] + clips			
-	finalClip = concatenate_videoclips(clips, method ='compose')
-	finalClip.write_videofile('POWERHOUR!.mp4',codec='libx264', audio_codec='aac',temp_audiofile='temp-audio.m4a', remove_temp=True,fps =30)
-	pwNumber =  len(os.listdir('completedPWs'))+1
-	os.rename('POWERHOUR!.mp4', 'completedPWs/POWERHOUR'+str(pwNumber)+'!.mp4')
+    titles = os.listdir('videos')
+    random.shuffle(titles) #randomizes order of the titles
+    clips = []
+    for title in titles: #creates the Moviepy clip object and adds it to the clip array
+        if title != '.DS_Store': #does not include random DS File
+            video = VideoFileClip('videos/' + title)
+            clips.append(video)
+    count = 0
+    for clip in clips: #adds a fade to the clips
+        if count != 0:
+            clip.crossfadein(1)
+        count += 1
+    titleClip = VideoFileClip('PowerHourIntro.mp4')
+    clips = [titleClip] + clips
+    finalClip = concatenate_videoclips(clips, method='compose') #concates the clips together
+    pwNumber = len(os.listdir('completedPWs')) + 1
+    #creates the clip and saves
+    finalClip.write_videofile('completedPWs/POWERHOUR' + str(pwNumber) +'!.mp4', codec='libx264', audio_codec='aac',
+                              temp_audiofile='temp-audio.m4a', remove_temp=True, fps=30)
+    
+
 def main():
-	combineVideos()
+    combineVideos()
+
 
 if __name__ == "__main__":
-	main()
-
-
-
-
+    main()
--- cropVideos.py
+++ cropVideos.py
@@ -2,29 +2,32 @@ from moviepy.editor import *
 import os
 
 
-def cropVideo(title,clipCut):
-	min,sec = clipCut
-	video = VideoFileClip('videos/'+title)
-	videoedited = video.subclip((min-1,sec),(clipCut))
-	logoOut = (ImageClip("keystone.png")
-      .set_duration(4)
-      .resize(height=200) # if you need to resize...
-      .margin(right=8, top=8, opacity=0) # (optional) logo-border padding
-      .set_pos(("center")))
-	logoIn = (ImageClip("keystone.png")
-      .set_duration(1)
-      .resize(height=200)
-      .margin(right=8, top=8, opacity=0) # (optional) logo-border padding
-      .set_pos(("center")))
-	beerCrack = AudioFileClip('audio/beercrack.mp3')
-	beerCrack = beerCrack.subclip(5,7)
-	beerCrack = beerCrack.volumex(2)
-	final = CompositeVideoClip([videoedited,logoIn.set_start(0).crossfadeout(.5),logoOut.set_start(56.00).crossfadein(.5)])
-	audioClip = CompositeAudioClip([final.audio.volumex(1),beerCrack.set_start(58.00)])
-	final.audio = audioClip
-	final.write_videofile(title,codec='libx264', audio_codec='aac',temp_audiofile='temp-audio.m4a', remove_temp=True)
-	os.rename(title, "videos/"+title)
-
-
-
-
+def cropVideo(title, clipCut):
+    min, sec = clipCut
+    video = VideoFileClip('videos/' + title)
+    # crops a 60 sec clip from 60s before clipcut
+    videoedited = video.subclip((min - 1, sec), (clipCut))
+    logoOut = (ImageClip("keystone.png")  # adds keystone fade in
+               .set_duration(4)
+               .resize(height=200)  # if you need to resize...
+               # (optional) logo-border padding
+               .margin(right=8, top=8, opacity=0)
+               .set_pos(("center")))
+    logoIn = (ImageClip("keystone.png")  # adds keystone fade out
+              .set_duration(1)
+              .resize(height=200)
+              # (optional) logo-border padding
+              .margin(right=8, top=8, opacity=0)
+              .set_pos(("center")))
+    # adds the beer crack sound
+    beerCrack = AudioFileClip('audio/beercrack.mp3')
+    beerCrack = beerCrack.subclip(5, 7)
+    beerCrack = beerCrack.volumex(2)
+    final = CompositeVideoClip([videoedited, logoIn.set_start(
+        0).crossfadeout(.5), logoOut.set_start(56.00).crossfadein(.5)])
+    audioClip = CompositeAudioClip(
+        [final.audio.volumex(1), beerCrack.set_start(58.00)])
+    final.audio = audioClip
+    final.write_videofile(title, codec='libx264', audio_codec='aac',
+                          temp_audiofile='temp-audio.m4a', remove_temp=True)
+    os.rename(title, "videos/" + title)
